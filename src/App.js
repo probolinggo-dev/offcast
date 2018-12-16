558a1faf1d7da45290e93f0908ec9a9ec67bba14
Ade Yahya <adeyahyaprasetyo@gmail.com>
2018-12-16 21:52:14 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+const EXTENSION_ID = 'amkjnhmbidmfoolfolmkacnailbbpdna';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <canvas />
+        <video />
+        <image />
+      </div>
+    );
+  }
+}
+
+export default App;
